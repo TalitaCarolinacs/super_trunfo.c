1b96a985b696295fd7948b673e804112ffeb5d71
TalitaCarolinacs <talitacarolinacs@outlook.com>
2025-04-23 19:58:27 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,8 +2,8 @@
 
 int main(){
 
-    char carta = 'A01';
-    char cart = 'B02';
+    char carta [4];
+    char cart [4];
     char escolha;
     char estado [20];
     char cidade [20];
@@ -19,8 +19,8 @@ int main(){
     //usando o printf para imprimir as informações
     
     printf("Digite o código da carta, ex A01\n");
-    scanf(" %c", &carta);
-    printf("A carta informada foi: %c\n", carta);
+    scanf("%3s", &carta);
+    printf("A carta informada foi: %s\n", carta);
     
     printf("Digite o estado, ex SP\n");
     scanf("%s", &estado);
@@ -29,7 +29,6 @@ int main(){
     printf("Digite a cidade:\n");
     scanf("%s", &cidade);
     printf("A cidade informada foi: %s\n", cidade);
-
 
     printf("Informar a População local:\n");
     scanf("%d", &populacao);
@@ -47,14 +46,14 @@ int main(){
     scanf("%f", &pib);
     printf("O PIB informado foi: %f\n", pib);
 
-    //Limpa o buffer
+    //Limpa o buffer, limpar as linhas
     printf("------------------------------------------\n");
 
     //Solicitando dados da outra carta com printf e scanf
 
     printf("Digite o código da carta, ex B02\n");
-    scanf(" %c", &cart);
-    printf("A carta informada foi: %c\n", cart);
+    scanf("%3s", &cart);
+    printf("A carta informada foi: %s\n", cart);
     
     printf("Digite o estado, ex SP\n");
     scanf("%s", &estado);
@@ -63,7 +62,6 @@ int main(){
     printf("Digite a cidade:\n");
     scanf("%s", &cidade);
     printf("A cidade informada foi: %s\n", cidade);
-
 
     printf("Informar a População local:\n");
     scanf("%d", &populacao);
@@ -82,32 +80,44 @@ int main(){
     printf("O PIB informado foi: %f\n", piB);
 
     printf("--------------------------------------------------\n");
-    //Menu de cartas
-    printf("As cartas apresentadas foram: → %c - %c ← \n", carta, cart);
+    printf("As cartas apresentadas foram: → A - B ← \n", carta, cart);  
     printf("--------------------------------------------------\n");
 
-
-    //a carta que tem a maior informação de PIB, será a carta ganhadora do jogo
-
+    //escolha a carta para ser a vencedora
     printf("Escolha uma Carta ► A ◄ ou ► B ◄ ?\n");
     scanf(" %c", &escolha);
-    printf("Sua escolha foi: %c", escolha);
-    
+    printf("Sua escolha foi: %c", escolha);   
     
     //Transformar as letras escolhidas em maiúscula se digitada minúscula
-    escolha = (escolha >= 'a' && escolha <= 'b') ? escolha - 32 : escolha;
+    escolha = (escolha >= 'a' && escolha <= 'b') ? escolha - 32 : escolha; 
 
+    //Menu de cartas
+    printf("\n>> Dados Carta A - Confira ↓:\n");
+    printf("A Carta escolhida: A\n");
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Pontos turísticos: %d\n", turismo);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("----------------------------------\n");
 
-    printf("A carta vencedora   '\('0')/'     %c \n", escolha);
-
-
+    printf("\n>> Dados Carta B, Confira ↓:\n");
+    printf("A Carta escolhida: B\n");
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Pontos turísticos: %d\n", turismo);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("----------------------------------\n");  
 
     //Apenas imprimir a escolha se for A ou B, senão imprimir erro
-    
     printf("Carta escolhida: %c\n", escolha);
     printf("Essa opção está %s\n", (escolha == 'A' || escolha == 'B')? "☺" : "░ERRADA|ERROR░");
+
+    printf("A carta vencedora   '\('0')/'     %c \n", escolha);
 
     return 0;
 
 }
-
